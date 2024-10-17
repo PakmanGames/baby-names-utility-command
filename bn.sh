eeee09cc62d80f21d4943112584c5357402d5f73
Andy Pak <im.andy.pak@gmail.com>
2024-10-17 14:24:34 -0400

    Create more input validations, check year and gender arguments

--- bn.sh
+++ bn.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# bn utility that returns the ranking for a given baby name in a given year based on gender
+# Andy Pak, McMaster University, 2024
 
 rank() {
     # TODO given name, year, and gender, return rank
@@ -17,7 +19,30 @@ help() {
 
 if [[ "$#" -eq 0 ]]
 then
+    echo "No arguments given" >&2
+    usage
+    exit 1
+elif [[ "$#" -ne 2 ]]
+then
     echo "Wrong number of arguments" >&2
     usage
     exit 1
+fi
+
+year="$1"
+gender="$2"
+# exit 2 if bad arguments
+
+if ! [[ "$year" =~ ^[0-9]{4}$ && "$year" -gt 1879 && "$year" -lt 2025 ]]
+then
+    echo "Error: Year must be 4-digit between 1880 to 2024" >&2
+    usage
+    exit 2
+fi
+
+if ! [[ "$gender" =~ ^[fFmMbB]$ ]]
+then
+    echo "Error: Gender can be f, m, F, M, b, or B" >&2
+    usage
+    exit 2
 fi
