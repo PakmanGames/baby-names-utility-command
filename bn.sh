ebfba5ba534a98236e02a1eee0f050c48632be60
Andy Pak <im.andy.pak@gmail.com>
2024-10-17 17:41:53 -0400

    Get total names that match gender and get entry for specific name

--- bn.sh
+++ bn.sh
@@ -2,6 +2,20 @@
 # bn utility that returns the ranking for a given baby name in a given year based on gender
 # Andy Pak, McMaster University, 2024
 
+get_ranking_for_gender() {
+    local name="$1"
+    local file="$2"
+    local gender="$3"
+
+    # lucy 44/942 in 1880
+    # cat ./us_baby_names/yob1880.txt | grep -P ",F," | wc -l
+    # cat ./us_baby_names/yob1880.txt | grep -P -i "^Lucy," | grep -P ",F,"
+    local total_names=$(cat "$file" | grep -P -i ",$gender," | wc -l)
+    local entry=$(cat "$file" | grep -P -i "^$name," | grep -P -i ",$gender,")
+    echo "total $gender names is $total_names" # testing
+    echo "the entry is $entry" # testing
+}
+
 rank() {
     local name="$1"
     local year="$2"
@@ -13,7 +27,14 @@ rank() {
         echo "No data for $year" >&2
         exit 4
     fi
-    # TODO
+    
+    if [[ "$gender" =~ [bB] ]]
+    then
+        get_ranking_for_gender "$name" "$file" "F"
+        get_ranking_for_gender "$name" "$file" "M"
+    else
+        get_ranking_for_gender "$name" "$file" "$gender"
+    fi
 }
 
 usage() {
